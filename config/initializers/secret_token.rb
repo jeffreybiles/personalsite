59ea163cf212cfc15ae0dbc10ca4479f99e1baeb
jeffreybiles <bilesjeffrey@gmail.com>
2012-05-18 02:58:28 -0500

    first commit.  Got high_voltage running.
    This will be a replacement for my all-html/css/js site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalsite::Application.config.secret_token = '06c479e282651c0cb03899c83bfd4b885e478d554dfda9fa19d21dc7f8bdbbe13b42e930c7a3284da88940e72db81cb874dc1e331526518ed2613d0c447d8a89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Personalsite::Application.config.session_store :cookie_store, key: '_personalsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Personalsite::Application.config.session_store :active_record_store
